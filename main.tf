b6b5cef24ad636e34092ee657828582071b0f8df
Kescefko <78681186+Kescefko@users.noreply.github.com>
2025-03-04 09:19:28 +0100

    Update location to germany west central

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 resource "azurerm_resource_group" "myterraformgroup" {
   name     = "myResourceGroup"
-  location = "SouthIndia"
+  location = "GermanyWestCentral"
 }
 # Create virtual network
 resource "azurerm_virtual_network" "myterraformnetwork" {
   name                = "myVnet"
   address_space       = ["10.0.0.0/16"]
-  location            = "SouthIndia"
+  location            = "GermanyWestCentral"
   resource_group_name = azurerm_resource_group.myterraformgroup.name
 }
 # Create subnet
@@ -19,14 +19,14 @@ resource "azurerm_subnet" "myterraformsubnet" {
 # Create public IPs
 resource "azurerm_public_ip" "myterraformpublicip" {
   name                = "myPublicIP"
-  location            = "SouthIndia"
+  location            = "GermanyWestCentral"
   resource_group_name = azurerm_resource_group.myterraformgroup.name
   allocation_method   = "Static"
 }
 # Create Network Security Group and rule
 resource "azurerm_network_security_group" "myterraformnsg" {
   name                = "myNetworkSecurityGroup"
-  location            = "SouthIndia"
+  location            = "GermanyWestCentral"
   resource_group_name = azurerm_resource_group.myterraformgroup.name
 
   security_rule {
@@ -44,7 +44,7 @@ resource "azurerm_network_security_group" "myterraformnsg" {
 # Create network interface
 resource "azurerm_network_interface" "myterraformnic" {
   name                = "myNIC"
-  location            = "SouthIndia"
+  location            = "GermanyWestCentral"
   resource_group_name = azurerm_resource_group.myterraformgroup.name
 
   ip_configuration {
@@ -62,7 +62,7 @@ resource "azurerm_network_interface_security_group_association" "example" {
 # Create virtual machine
 resource "azurerm_linux_virtual_machine" "myterraformvm" {
   name                  = "myVM"
-  location              = "SouthIndia"
+  location              = "GermanyWestCentral"
   resource_group_name   = azurerm_resource_group.myterraformgroup.name
   network_interface_ids = [azurerm_network_interface.myterraformnic.id]
   size                  = "Standard_A2_v2"
